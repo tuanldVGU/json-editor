4b1f96bf879aab95b409c79d8d30c557a043d0eb
tuanldVGU <37131827+tuanldVGU@users.noreply.github.com>
2019-07-16 21:37:40 +0700

    7.Autocomplete for outline (#8)

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -59,7 +59,7 @@
 
 <script>
 import _ from 'lodash';
-import { eventBus } from '../../main';
+import { eventBus } from '../../main.js';
 
 var keyCtrl = require('./assets/KeyController.js')
 var util = require('../common_assets/util.js');
