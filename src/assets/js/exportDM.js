1e07a4e2598e7e86217aeb78404a28d075de12e0
tuanldVGU <37131827+tuanldVGU@users.noreply.github.com>
2019-09-23 20:05:50 +0700

    48.tweaks (#49)
    Enhance UI

--- src/assets/js/exportDM.js
+++ src/assets/js/exportDM.js
@@ -11,28 +11,28 @@ exports.toSQL = function (json){
 }
 
 function classTable (obj){
-  let output = "CREATE TABLE " + obj['class'] + " (\n" + obj['class'] + "_id int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY";
+  let output = "CREATE TABLE " + obj['class'] + " (\n\t" + obj['class'] + "_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY";
   let fk = [];
   obj['attributes'].forEach(element => {
     let type = "";
     switch (element['type']){
       case 'String':
-          type = "varchar(255)";
+          type = "VARCHAR(255)";
         break;
       case 'Integer':
-          type = "int(11)";
+          type = "INT(11)";
         break;
       default:
-          type = "int(11)";
+          type = "INT(11)";
           fk.push("FOREIGN KEY (" + element['type'] + "_id) REFERENCES " + element['type'] +"("+ element['type'] +"_id)");
         break;
     }
-    output += ",\n"+element['name'] + " " + type + " DEFAULT NULL";
+    output += ",\n\t"+element['name'] + " " + type + " DEFAULT NULL";
   });
   fk.forEach(element => {
     output += ",\n"+element;
   });
-  output += "\n);";
+  output += "\n) ENGINE=InnoDB;";
   return output;
 }
 
@@ -40,22 +40,24 @@ function addClassAttribute(class_name,name,type){
   let end = ";";
   switch (type){
     case 'String':
-        end = "varchar(255);";
+        end = "VARCHAR(255);";
       break;
     case 'Integer':
-        end = "int(11);";
+        end = "INT(11);";
       break;
     default:
-        end = "int(11);\n ALTER TABLE " + class_name + "ADD FOREIGN KEY fk_" + class_name +"_";
+        end = "INT(11);\n ALTER TABLE " + class_name + "ADD FOREIGN KEY fk_" + class_name +"_";
       break;
   }
   return "ALTER TABLE " +  class_name + " ADD COLUMN at " + end;
 }
 
 function associationTable(obj){
-  return "CREATE TABLE " + obj['association'] + " (\n" + obj['ends'][0] + " int,\n "+ obj['ends'][1] + " int, \n"+
-        " FOREIGN KEY fk_"+ obj['classes'][0] + "_" + obj['ends'][0] + "(" +  obj['ends'][0] + ")\n"+
-        " REFERENCES "+ obj['classes'][0] +"("+  obj['classes'][0] +"_id), \n" +
-        " FOREIGN KEY fk_"+ obj['classes'][1] + "_" + obj['ends'][1] + "(" +  obj['ends'][1] + ")\n"+
-        " REFERENCES "+ obj['classes'][1] +"("+  obj['classes'][1] +"_id));";
+  return "CREATE TABLE " + obj['association'] + 
+        " (\n\t" + obj['ends'][0] + " INT(11),\n\t"
+        + obj['ends'][1] + " INT(11), \n"+
+        "\tCONSTRAINT fk_"+ obj['classes'][1] + "_" + obj['ends'][0] + " FOREIGN KEY (" +  obj['ends'][0] + ")\n"+
+        "\tREFERENCES "+ obj['classes'][1] +"("+  obj['classes'][1] +"_id), \n" +
+        "\tCONSTRAINT fk_"+ obj['classes'][0] + "_" + obj['ends'][1] + " FOREIGN KEY (" +  obj['ends'][1] + ")\n"+
+        "\tREFERENCES "+ obj['classes'][0] +"("+  obj['classes'][0] +"_id)\n) ENGINE=InnoDB;";
 }
--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -43,6 +43,18 @@ require('brace/ext/language_tools');
 var error = '<i class="fas fa-exclamation-triangle"></i>';
 var no_error  = '<i class="fas fa-check-circle"></i>';
 var debounce_interval = 500;
+
+var semantic_format = {
+	class: "\"class\": \"\", \"attributes\" : []",
+	association: "\"association\": \"\",\"ends\" : [],\"classes\" : []",
+	type_name: "\"name\": \"\",\"type\" : \"\""
+}
+
+var semantic_format_afteCursor = {
+	class: 10,
+	association: 16,
+	type_name: 9
+}
 export default {
   	name: 'input-component',
 	data: function(){
@@ -90,13 +102,38 @@ export default {
 					var wordList = me.getAutocompleteList();
 					callback(null, wordList.map(function(word) {
 						// auto close "" if users not already do it
-						let close = '';
+						let close = ''; let start = '';
 						let line = session.getLine(pos.row);
 						if (line.charAt(pos.column) != "\"" && line.charAt(pos.column-prefix.length-1) == "\"") close = "\"";
+						if (line.charAt(pos.column) != "\"" && line.charAt(pos.column-prefix.length-1) != "\"") { start = "\""; close = "\""; } 
 						return {
 							caption: word,
-							value: word+close,
+							value: start+word+close,
 							meta: "static"
+						};
+					}));
+				}
+			}
+			let formatWordCompleter = {
+				getCompletions: function(editor, session, pos, prefix, callback) {
+					var wordList = me.getFormList();
+					callback(null, wordList.map(function(word) {
+						// auto close {} if users not already do it
+						let close = ''; let start = '';
+						let line = session.getLine(pos.row);
+						if (line.charAt(pos.column) != "}" && line.charAt(pos.column-prefix.length-1) == "{") close = "}";
+						if (line.charAt(pos.column) != "}" && line.charAt(pos.column-prefix.length-1) != "{") { start = "{"; close = "}"; }
+						return {
+							caption: word,
+							value: start+semantic_format[word]+close,
+							meta: "format",
+							completer: {
+								insertMatch: function(editor,data){
+									editor.completer.insertMatch({value: data.value});
+									let pos = editor.selection.getCursor();
+									editor.gotoLine(pos.row + 1, pos.column - data.value.length + semantic_format_afteCursor[word]);
+								}
+							}
 						};
 					}));
 				}
@@ -107,11 +144,38 @@ export default {
 				enableLiveAutocompletion: true
 			});
 			langTools.setCompleters([staticWordCompleter]);
+			langTools.addCompleter(formatWordCompleter);
+			this.editor.commands.addCommand({
+				name: "nextItem",
+				bindKey: {win: "Tab",mac: "Tab"},
+				exec: function(editor) {
+					let pos = editor.getCursorPosition();
+					let before = editor.session.getLines(0,pos.row-1).join('');
+					before += editor.session.getLine(pos.row).substring(0,pos.column);
+					let isOdd = (before.match(/"/g) || []).length %2 != 0;
+					if ( isOdd || ((before.match(/\[/g) || []).length - (before.match(/\]/g) || []).length > 0)){
+						let check = editor.session.getLine(pos.row).substr(pos.column);
+						let count = 0; let open = false; let exit= false;
+						for (var i =0; i< check.length; i++){
+							if (check.charAt(i) == "\"") count++;
+							if (check.charAt(i) == "[") open=true;
+							if ((count == 3) || (check.charAt(i) == ']' && open) || (!isOdd && count == 2)) {
+								editor.gotoLine(pos.row+1,pos.column+i);
+								exit=true;
+								break;
+							}
+						}
+						if (!exit) editor.indent();
+					} else { editor.indent(); }
+				},
+				multiSelectAction: "forEach",
+				scrollIntoView: "selectionPart"
+			});
 		},
 		setEvents: function(){
 			var me = this;
 			this.editor.on('change',_.debounce(function(e){
-				if (e.lines[0] == "\"\""){
+				if (e.lines[0] == "\"\"" || e.lines[0] == "{" ||  e.lines[0] == "{}"){
 					me.editor.commands.byName.startAutocomplete.exec(me.editor);
 				};
 				let val = me.editor.getValue();
@@ -155,11 +219,17 @@ export default {
 		getAutocompleteList: function(){
 			let wordList = ['class','association','super','ends','attributes','name','type','classes'];
 			let caretPos = this.editor.getCursorPosition();
+			let isClose = 0;
 			for (var row = caretPos.row; row >0; row--){
-				let inElement = this.editor.session.getLine(row).replace(/\s/g,'');
+				let inElement = this.editor.session.getLine(row);
+				if (row == caretPos.row) inElement = inElement.substr(0,caretPos.column);
+				inElement.replace(/\s/g,'');
 				inElement = inElement.split("\"");
-				for (var i=(inElement.length-1); i>0; i--){
-					if (wordList.includes(inElement[i])){
+				if (row == caretPos.row && inElement.length >= 2 &&inElement[(inElement.length-2)].includes(':')) return [];
+				for (var i=(inElement.length-1); i>=0; i--){
+					if (inElement[i].includes('}') || inElement[i].includes('{') ) isClose = this.bracket_count(inElement[i],isClose);
+					if (isClose > 0) continue; 
+					if (wordList.includes(inElement[i]) && inElement[i+1] != ''){
 						switch (inElement[i]){
 							case 'class':
 								return ['attributes','super'];
@@ -174,14 +244,41 @@ export default {
 								return ['String','Integer'];
 								break;
 							default:
-								return wordList;
+								return [];
 								break;
 						}
 						break;
 					}
 				}
 			}
-			return wordList;
+			return ['class','association'];
+		},
+		getFormList: function(){
+			let wordList = ['class','association','attributes'];
+			let caretPos = this.editor.getCursorPosition();
+			let isClose = 0;
+			for (var row = caretPos.row; row > 0; row--){
+				let inElement = this.editor.session.getLine(row);
+				if (row == caretPos.row) inElement = inElement.substr(0,caretPos.column);
+				inElement.replace(/\s/g,'');
+				inElement = inElement.split("\"");
+				for (var i=(inElement.length-1); i>=0; i--){
+					if (inElement[i].includes('}') || inElement[i].includes('{') ) isClose = this.bracket_count(inElement[i],isClose);
+					if (isClose > 0) 	continue; 
+					if (wordList.includes(inElement[i]) && inElement[i+1] != ''){
+						switch (inElement[i]){
+							case 'attributes':
+								return ['type_name'];
+								break;
+							default:
+								return [];
+								break;
+						}
+						break;
+					}
+				}
+			}
+			return ['class','association'];
 		},
 		checkSyntax: function(){
 			let pkg = this.getValue();
@@ -193,6 +290,15 @@ export default {
 						// error callback
 						console.log(response);
 					})
+		},
+		bracket_count: function(str,count){
+			let caretPos = this.editor.getCursorPosition();
+			let start = str.length-1;
+			for (var i= start; i>=0; i--){
+				if (str[i] == '{' && count >0) count--;
+				if (str[i] == '}') count++;
+			}
+			return count;
 		}
 	},
 	watch: {
@@ -205,6 +311,9 @@ export default {
 			if (val.msg) this.has_error = error;
 			else this.has_error = no_error;
 			eventBus.$emit('jsonSyntax_error',val);
+		},
+		'options.View_mode': function(val){
+			this.editor.setReadOnly(val);
 		}
 	},
 	created(){
